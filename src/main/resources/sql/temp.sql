181d41fdabdc6a1bbeefc8e742c3546cbb5811ab
Karpusha Serhii <kapcep@gmail.com>
2021-12-15 11:23:02 +0200

    populate schedule items data queries

--- src/main/resources/sql/temp.sql
+++ src/main/resources/sql/temp.sql
@@ -5,3 +5,6 @@ from classroom
 select * from student_groups;
 
 select student_groups.id,student_groups.group_name, faculties.faculty_name from student_groups join faculties on student_groups.faculty_id = faculties.id;
+
+
+insert into  schedule_items (begin_time, end_time, lesson_name,classroom_id, student_group_id, teacher_id) values ('2021-12-01 08:30','2021-12-01 09:25','Algorithms',3,1,1);
