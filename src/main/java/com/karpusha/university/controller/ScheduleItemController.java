a3527b93e2e80418a93d32999f55594278f7379d
Serhii Karpusha <kapcep@gmail.com>
2021-12-16 22:38:51 +0200

    creating list of student group in view

---
+++ src/main/java/com/karpusha/university/controller/ScheduleItemController.java
@@ -0,0 +1,29 @@
+package com.karpusha.university.controller;
+
+import com.karpusha.university.entity.StudentGroup;
+import com.karpusha.university.service.StudentGroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ScheduleItemController {
+    @Autowired
+    StudentGroupService studentGroupService;
+
+
+    @RequestMapping("/getAllScheduleItems")
+    public String showAllScheduleItems(Model model) {
+        List<StudentGroup> studentGroups = new ArrayList<>();
+        studentGroups.add(studentGroupService.getStudentGroup(1));
+        studentGroups.add(studentGroupService.getStudentGroup(2));
+        studentGroups.add(studentGroupService.getStudentGroup(3));
+        model.addAttribute("studentGroups", studentGroups);
+        return "all-schedule-items";
+    }
+
+}
