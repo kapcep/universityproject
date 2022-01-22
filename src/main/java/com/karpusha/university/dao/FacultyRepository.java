93fc56950800306f832cf7bb97f5d1c4dc7308ec
Serhii Karpusha <kapcep@gmail.com>
2022-01-22 20:54:12 +0200

    working with implementing JpaRepository

---
+++ src/main/java/com/karpusha/university/dao/FacultyRepository.java
@@ -0,0 +1,7 @@
+package com.karpusha.university.dao;
+
+import com.karpusha.university.entity.Faculty;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FacultyRepository extends JpaRepository <Faculty, Integer>{
+}
