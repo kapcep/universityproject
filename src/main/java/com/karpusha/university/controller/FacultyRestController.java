6d2c84e2fd129b7f19afa2dc15124153c0ee7177
Karpusha Sergi <kapcep@gmail.com>
2022-05-17 17:23:59 +0300

    crossOrigin

--- src/main/java/com/karpusha/university/controller/FacultyRestController.java
+++ src/main/java/com/karpusha/university/controller/FacultyRestController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/faculties")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class FacultyRestController {
 
     @Autowired
